f36a3a99f02f33b154dd79b67806eb13078ccfdb
Andreas <pingis00@hotmail.com>
2024-10-20 21:03:29 +0200

    Set up initial layout with centered container and button, added SCSS structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Home from './views/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Infinite Squares Generator</h1>
+      <Home />
     </div>
   );
 }
